c539f9435b013a2308fd013528c24e0665b78b42
Leandro Sombra <leandro.sombra@vagas.com.br>
2015-12-10 19:55:23 -0200

    bootstrap

--- app/gulp/sass.js
+++ app/gulp/sass.js
@@ -7,4 +7,8 @@ gulp.task('sass', ['fonts'], function(){
 	gulp.src('./src/assets/sass/**/main.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('../dist/assets/css/'));
+
+	gulp.src('./bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('../dist/assets/css/'));
 });
